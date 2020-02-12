bf506a03b8aa453a9db01aa3b72adbc2f5e219bc
Marc Backes <mab@mab.lu>
2020-02-12 11:54:22 +0100

    commented the js code

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -1,4 +1,5 @@
-const getFieldsToCheck = (x, y) => {
+// Fields that are around a specific field, defined by x, y
+const neighborFields = (x, y) => {
     return [
         [x - 1, y - 1],
         [x, y - 1],
@@ -12,19 +13,20 @@ const getFieldsToCheck = (x, y) => {
 }
 
 class Game {
+    // Creates the game, but doesn't initialize the mines yet, because they are placed depending on the first click of the user
     constructor(dimensions = 10, difficulty = 1) {
+        // In the worst case, there are mines everywhere, except the first clicked field and it's neighbor fields
         const maximumAllowedMines = dimensions * dimensions - 9
-        window.__game = this
-        this.dimensions = dimensions
 
+        // Calculate mine count depending on the number field dimensions and difficulty
         this.mineCount = Math.floor(
             dimensions * difficulty <= maximumAllowedMines ? dimensions * difficulty : maximumAllowedMines
         )
 
+        this.dimensions = dimensions
         this.gameCreated = true
-        this.gameStarted = false
 
-        // Fill with empty objects
+        // Initialize fields, fill them with empty objects
         this.fields = new Array(dimensions).fill({})
         window.__fields = this.fields
         this.fields.forEach((field, index) => {
@@ -32,34 +34,45 @@ class Game {
         })
     }
 
+    // When the user makes their first click, the game starts
+    // The positioning of the mines is determined based on this first clicked field position
     startGame(x, y) {
         this.startingPoint = [x, y]
-        // Fill field with mines
+        this.gameStarted = true
+
+        // Set mines within the field
         for (let i = 0; i < this.mineCount; i++) {
             const [randomX, randomY] = this.getRandomFreeField()
             this.fields[randomX][randomY] = { hasMine: true }
         }
-
-        this.gameStarted = true
     }
 
-    sleep() {
-        return new Promise(resolve => setTimeout(resolve, 2000))
-    }
-
+    // Determines if a mine can be placed on the given position
+    // Reasons for not being able to place a mine
+    //  - It's the first clicked field
+    //  - It's one of the neigboring fields of the first clicked field
+    //  - There is already a mine on that field
     canPlaceMine(x, y) {
         const [startX, startY] = this.startingPoint
+
+        // Is it the first clicked field?
         const isStartingPoint = x === startX && y === startY
-        const isAroundStartingPoint = getFieldsToCheck(startX, startY).some(([_x, _y]) => {
+
+        // Is it a neighboring field?
+        const isNeighboringField = neighborFields(startX, startY).some(([_x, _y]) => {
             return x == _x && y == _y
         })
-        const hasMine = this.fields[x][y].hasMine === true
 
-        return !isAroundStartingPoint && !hasMine && !isStartingPoint
+        // Does it already have a mine?
+        const hasAlreadyMine = this.fields[x][y].hasMine === true
+
+        return !isNeighboringField && !hasAlreadyMine && !isStartingPoint
     }
 
+    // Generate a random field. Generate random field positions until a valid field is found (determined by canPlaceMine)
     getRandomFreeField() {
         let randomX, randomY
+        // Repeat until valid field
         do {
             randomX = Math.floor(Math.random() * this.dimensions)
             randomY = Math.floor(Math.random() * this.dimensions)
@@ -68,60 +81,69 @@ class Game {
         return [randomX, randomY]
     }
 
+    // Returns a list of flagged fields
     getFlaggedFields() {
         return this.fields.flat().filter(field => field.isFlagged)
     }
 
-    getReveiledFields() {
-        return this.fields.flat().filter(field => field.isReveiled)
+    // Returns a list of revealed fields
+    getRevealedFields() {
+        return this.fields.flat().filter(field => field.isRevealed)
     }
 
+    // Returns the number of mines placed in the field
     getMineCount() {
         return this.mineCount
     }
 
+    // Returns whether the game is won.
+    // A game is won when both is true:
+    //  - All non-mined fields are revealed
+    //  - All mined fields are flagged
     isGameWon() {
-        return (
-            this.getReveiledFields().length + this.getFlaggedFields().length === this.dimensions * this.dimensions &&
-            this.getMineCount() - this.getFlaggedFields().length === 0
-        )
+        const nonMinedFields = this.fields.flat().filter(field => !field.hasMine)
+        const minedFields = this.fields.flat().filter(field => field.hasMine)
+
+        return nonMinedFields.every(field => field.isRevealed) && minedFields.every(field => field.isFlagged)
     }
 
+    // Returns when the game is list
+    // A game is lost when a mine has been revealed
     isGameOver() {
-        return this.getReveiledFields().filter(field => field.hasMine).length > 0
+        return this.getRevealedFields().filter(field => field.hasMine).length > 0
     }
 
-    isGameStarted() {
-        return this.gameStarted
-    }
-
+    // Returns if the game has been initialized
     isGameCreated() {
         return this.gameCreated
     }
 
-    // Calculate how many neighbor fields have mines
+    // Calculates how many neighbor fields have mines
     getHint(x, y) {
-        const fieldsToCheck = getFieldsToCheck(x, y)
-
-        const neighborsWithMine = fieldsToCheck.reduce(
+        const neighborsWithMine = neighborFields(x, y).reduce(
             (accumulator, [x, y]) => accumulator + (this.getField(x, y) && this.getField(x, y).hasMine ? 1 : 0),
             0
         )
-
         return neighborsWithMine
     }
 
+    // Reveals a field
+    //  - Sets the revealed status to the field (isRevealed)
+    //  - If the field is not neighboring a mine, recursively reveal the surrounding fields
     reveal(x, y) {
+        // If the game has not already been started, it gets started (first click)
         if (!this.gameStarted) this.startGame(x, y)
 
         const hint = this.getHint(x, y)
-        this.fields[x][y] = { ...this.fields[x][y], isReveiled: true, hint }
-        const fieldsToCheck = getFieldsToCheck(x, y)
+        this.fields[x][y] = { ...this.fields[x][y], isRevealed: true, hint }
 
+        // This is the stop-condition for the recursive call
         if (hint === 0) {
-            fieldsToCheck.forEach(([x, y]) => {
+            // Check all neighboring fields
+            neighborFields(x, y).forEach(([x, y]) => {
                 const field = this.getField(x, y)
-                if (field !== null && !field.isReveiled && !field.hasMine) {
+                // Reveal if the field exists, is not revealed yet and has no mine
+                if (field !== null && !field.isRevealed && !field.hasMine) {
                     this.reveal(x, y)
                 }
             })
@@ -129,39 +151,47 @@ class Game {
         return this.fields[x][y]
     }
 
+    // Resolve the entire game. All fields are being revealed
     resolve() {
         this.fields.forEach((rowField, rowFieldIndex) => {
             rowField.forEach((columnField, columnFieldIndex) => {
                 const hint = this.getHint(columnFieldIndex, rowFieldIndex)
                 this.fields[columnFieldIndex][rowFieldIndex] = {
                     ...this.fields[columnFieldIndex][rowFieldIndex],
-                    isReveiled: true,
+                    isRevealed: true,
                     hint
                 }
             })
         })
     }
 
+    // Flag a field. (Set the isFlagged property)
     flag(x, y) {
-        if (!this.fields[x][y].isReveiled) {
+        if (!this.fields[x][y].isRevealed) {
             this.fields[x][y] = { ...this.fields[x][y], isFlagged: true }
         }
         return this.fields[x][y]
     }
 
+    // Unflag a field. Opposite of flag()
     unflag(x, y) {
         this.fields[x][y] = { ...this.fields[x][y], isFlagged: false }
         return this.fields[x][y]
     }
 
+    // Toggle the flagged-status of a field
     toggleFlag(x, y) {
         this.fields[x][y].isFlagged ? this.unflag(x, y) : this.flag(x, y)
     }
 
+    // Return a field with the given position
     getField(x, y) {
+        // Only return a field if it's in bounds
         if (x >= 0 && x < this.dimensions && y >= 0 && y < this.dimensions) {
             return this.fields[x][y]
-        } else {
+        }
+        // Return null if it doesn't exist (out of bounds)
+        else {
             return null
         }
     }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import '@fortawesome/fontawesome-free/js/all'
 
 import { Game } from './Game'
 
+// Elements in the DOM that need to be accessed during the game
 const gameElement = document.getElementById('game')
 const boardElement = document.getElementById('board')
 const boardStatus = document.getElementById('boardStatus')
@@ -12,14 +13,17 @@ const minesLeftHeader = document.getElementById('minesLeftHeader')
 const minesLeftText = document.getElementById('minesLeft')
 const gameStartHeader = document.getElementById('game-start')
 
+// The game variable. Here lies all the logic
 let game
 
+// Little bit self-promotion never hurt anyone 😉
 console.log('🔥 Build with love by Marc Backes 🔥')
 console.log('   👉 Website: https://marc.dev')
 console.log('   👉 Twitter: https://twitter.com/_marcba')
 console.log('   👉 Codepen: https://codepen.io/_marcba')
 console.log('   👉 Github: https://github.com/themarcba')
 
+// Reveal a field for the selected position
 const revealField = (x, y) => {
     game.reveal(x, y)
     if (game.isGameOver()) {
@@ -27,61 +31,82 @@ const revealField = (x, y) => {
     }
 }
 
+// Toggle the flag-status on a field for the selected position
 const toggleFlagField = (x, y) => {
     game.toggleFlag(x, y)
     updateUI()
 }
 
+// When the game is over (lost)
 const gameOver = () => {
     gameElement.classList.add('over')
     game.resolve()
 }
 
+// When the game has been won
 const gameWon = () => {
     gameElement.classList.add('won')
 }
 
+// Create a new game
 const newGame = () => {
+    // A 5x5 board is too small for the mine-placing rules in the Game class
     if (dimensionInput.value < 5) return alert('There must be at least a field dimension of 5')
 
+    // Read desired board dimensions from the input field
+    const dimensions = parseInt(dimensionInput.value)
+
+    // Get the selected level from the emoji bar (radio buttons) (👶,😎,😐,🥵,😈)
     const level = document.querySelector('input[name="level"]:checked').value
 
+    // Remove winning or losing state of the board
     gameElement.classList.remove('over')
     gameElement.classList.remove('won')
 
-    const dimensions = parseInt(dimensionInput.value)
+    // Create the Game object which contains all the logic
     game = new Game(dimensions, level)
+
+    // Set CSS size for the board
     boardElement.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`
 
+    // Empty board, because new elements will be created
     boardElement.innerHTML = ''
 
-    // Create initial fields
-    game.fields.forEach((rowField, rowFieldIndex) => {
-        rowField.forEach((columnField, columnFieldIndex) => {
-            // Create field
+    // Create initial field elements
+    game.fields.forEach((rowField, rowFieldIndex) => { // Rows
+        rowField.forEach((columnField, columnFieldIndex) => { // Columns
+            
+            // Create & initialize a field element
             let newField = document.createElement('div')
             newField.id = `field_${rowFieldIndex}_${columnFieldIndex}`
             newField.className = 'field'
 
-            // Left click on field
+            // Left click event on field (reveal)
             newField.addEventListener('click', ev => {
                 revealField(rowFieldIndex, columnFieldIndex)
                 updateUI()
             })
 
-            // Right click on field
+            // Right click event on field (flag)
             newField.addEventListener('contextmenu', ev => {
                 ev.preventDefault()
                 toggleFlagField(rowFieldIndex, columnFieldIndex)
                 updateUI()
             })
 
+            // Append created field to board
             boardElement.appendChild(newField)
         })
     })
 
+    // Update the interface!
     updateUI()
 }
+
+// Updates the interface
+//  - Reads fields and sets CSS classes and content depending on field state
+//  - Checks if game is won or lost to display the respective messages
+//  - Hides "mines left" indicator when game is over
 const updateUI = () => {
     minesLeftText.innerText = game.getMineCount() - game.getFlaggedFields().length
     game.fields.forEach((rowField, rowFieldIndex) => {
@@ -89,7 +114,7 @@ const updateUI = () => {
             const fieldElement = document.getElementById(`field_${rowFieldIndex}_${columnFieldIndex}`)
             const field = game.getField(rowFieldIndex, columnFieldIndex)
 
-            if (field.isReveiled) {
+            if (field.isRevealed) {
                 fieldElement.className = 'reveiled field'
                 if (field.hasMine) {
                     fieldElement.classList = 'mine field'
@@ -117,7 +142,9 @@ const updateUI = () => {
     gameStartHeader.style.display = game.isGameCreated() ? 'none' : 'block'
 }
 
+// Add event listeners
 const attachEvents = () => {
+    // "New Game" Button
     newGameButton.addEventListener('click', newGame)
 }
 
